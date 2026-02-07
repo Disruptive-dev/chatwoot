bb3974a283b6a9e481c1089d5674b4bff783e57e
Disruptive-dev <pablo@disruptive-sw.com>
2026-02-07 16:47:48 -0300

    Update Branding.vue

--- app/javascript/shared/components/Branding.vue
+++ app/javascript/shared/components/Branding.vue
@@ -57,15 +57,15 @@ export default {
     class="px-0 py-3 flex justify-center"
   >
     <a
-      :href="https://optimia.disruptive-sw.com"
-      rel="noreferrer noopener nofollow"
+      href="https://optimia.disruptive-sw.com"
+      rel="noreferrer noopener"
       target="_blank"
       class="branding--link text-n-slate-11 hover:text-n-slate-12 cursor-pointer text-xs inline-flex grayscale-[1] hover:grayscale-0 hover:opacity-100 opacity-90 no-underline justify-center items-center leading-3"
     >
       <img
         class="ltr:mr-1 rtl:ml-1 max-w-3 max-h-3"
         :alt="globalConfig.brandName"
-        :src="/brand-assets/logo.svg"
+        src="/brand-assets/logo.svg"
       />
       <span>
         {{ replaceInstallationName($t('POWERED_BY')) }}
